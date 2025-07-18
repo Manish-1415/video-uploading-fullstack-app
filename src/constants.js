4915a22f86ecfccdd61d958381fd4c86a66ffe41
Manish-1415 <manishshinde1415@gmail.com>
2025-07-18 18:33:29 +0530

    Added User and Video models , and also use jwt and bcrypt

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,5 @@
 export const DB_NAME = "videotube"
 // this is simply a database name , instead of writing it in .env here we are writing , it is nothing like here only we can do this it is simple a variable that have a value that's it.
+
+// Here we store fixed values like database name , roles like USER : "user", ADMIN : "ADMIN"
+// Why we store here because it is also separation of concern , while using fixed values inside our project it distubrs code flow , for this we write hardcoded values inside constant.js
---
+++ src/models/user.models.js
@@ -0,0 +1,102 @@
+import mongoose, { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // index:true is a better/optimize way to use searching for a field inside database
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    fullname: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
+
+    avatar: {
+      type: String, // using cloudnarry
+      required: true,
+    },
+
+    coverImage: {
+      type: String, // using cloudnarry
+      required: true,
+    },
+
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+
+    refreshToken: {
+      type: String,
+    },
+  },
+
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+
+userSchema.methods.comparePassword = async function(password) {
+   return await bcrypt.compare(password, this.password )
+}
+
+userSchema.methods.generateAccessToken = function() {
+    jwt.sign(
+        {
+            _id: this._id,                                            
+            email: this.email,
+            fullName: this.fullName
+        },
+        // Here we need our access token's secret key 
+        process.env.ACCESS_TOKEN_SECRET ,
+        {
+            // and also set the deadline inside object
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function() {
+    jwt.sign(
+        {
+            _id: this._id                                           
+        },
+        // Here we need our refresh token's secret key
+        process.env.REFRESH_TOKEN_SECRET ,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = new model("User", userSchema);
